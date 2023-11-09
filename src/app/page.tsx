449356410d67e9e9dd3ffed4243dc2a3b6ff5781
UltrViolet <endgameviable@gmail.com>
2023-11-09 15:02:55 -0500

    test route dictionary on build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import EntryListLayout from '@/layouts/entryList'
+import { generateRouteDictionary } from '@/site/generateRoutes'
 import { generateLatestEntries } from '@/site/generateLatestEntries'
 
 export default async function Home() {
+  // I only want this function to run exactly once
+  // at build time on the server:
+  generateRouteDictionary()
+
   const entries = await generateLatestEntries()
   const content = 'The home page with the latest content entries.'
   return (
---
+++ src/site/generateRoutes.ts
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import Entry from "@/data/interfaces/entry"
+import { MATCH_ALL_ENTRIES } from "@/data/interfaces/queryFilter"
+import { getSections } from "@config/site-config"
+
+type RouteDictionary = {
+    [key: string]: Entry
+}
+
+export async function generateRouteDictionary(): Promise<RouteDictionary> {
+    console.log("scanning for content")
+    const promises: Promise<Entry[]>[] = []
+    for (const section of getSections()) {
+        promises.push(
+            section.provider.queryEntries(MATCH_ALL_ENTRIES)
+        )
+    }
+    const routeMap: RouteDictionary = {}
+    await Promise.all(promises)
+        .then((all) => {
+            console.log(`scanned ${all.length} sections`)
+            all.forEach((entries) => 
+                entries.forEach((entry) =>
+                    routeMap[entry.route] = entry
+                )
+            )
+        })
+        .catch((error) => console.log(error))
+    console.log("finished scanning for content")
+    console.log(`found ${routeMap.length} routes`)
+    const json = JSON.stringify(routeMap)
+    fs.writeFileSync('./public/routes.json', json)
+    return routeMap
+}
