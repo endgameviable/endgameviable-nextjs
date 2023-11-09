75a009393df36914e85d3ed80a1263e4a2a0f6d5
UltrViolet <endgameviable@gmail.com>
2023-11-09 12:31:10 -0500

    middleware

--- src/data/interfaces/fileDataSource.ts
+++ src/data/interfaces/fileDataSource.ts
@@ -1,6 +1,16 @@
 import Entry from "./entry"
 
-// Describes a generic file interface.
+// An interface to index all the entry routes within a file,
+// and to retrieve all the entries within that file.
+// The file might return one entry (e.g. a markdown file),
+// or it might return multiple entries (e.g. a YAML file).
+// The routes should describe how to fetch each entry.
+// Routes are typically constructed as a path and a slug.
+// e.g. posts/gaming/2023/10/my_gaming_post
+// e.g. movies/1931/dracula
+// The first part of the route determines what kind of data.
+// The final part of the route is a unique slug.
+// All routes must be unique across the entire site.
 export interface FileDataSource {
     //getEntry(route: string): Promise<Entry>
     getRoutes(): Promise<string[]>
