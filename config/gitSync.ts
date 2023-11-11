a1ca004930fd110b44ae01a6dec7174536f074ec
UltrViolet <endgameviable@gmail.com>
2023-11-10 20:50:36 -0500

    more git log messages

--- config/gitSync.ts
+++ config/gitSync.ts
@@ -5,13 +5,13 @@ import path from 'path';
 import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
 
 export async function syncHugoContentDir() {
-    console.log("running build tasks")
     await syncRepo('endgameviable-hugo',
         `https://${process.env.CODECOMMIT_USER}:${process.env.CODECOMMIT_PASS}@${process.env.CODECOMMIT_REPO}`)
 }
 
 // Clone and/or pull from a remote git repo
 async function syncRepo(dirName: string, remoteUrl: string) {
+    console.log("syncing repo", remoteUrl)
     const localDir = path.join(process.cwd(), "content-remote", dirName)
     const options: Partial<SimpleGitOptions> = {
         baseDir: path.join(process.cwd(), "content-remote"),
@@ -22,14 +22,18 @@ async function syncRepo(dirName: string, remoteUrl: string) {
     const git: SimpleGit = simpleGit(options);
   
     if (!fs.existsSync(localDir)) {
+        console.log("local directory doesn't exist, cloning repo")
         try {
             // Clone the remote Git repository
             await git.clone(remoteUrl, localDir);
             console.log('Repository cloned successfully.');
         } catch (error) {
-            //console.error('Error cloning repository:', error);
+            console.error('Error cloning repository:', error);
+            return
         }
     }
+
+    console.log("pulling latest changes from repo")
     git.cwd(localDir)
     git.pull()
 }
