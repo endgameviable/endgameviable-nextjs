01b67799125d39a18895cf75784d92ee745658b5
UltrViolet <endgameviable@gmail.com>
2023-11-16 17:33:19 -0500

    will these credentials work in amplify?

--- config/siteConfig.ts
+++ config/siteConfig.ts
@@ -1,5 +1,5 @@
+import { safeStringify } from '@/types/strings';
 import { S3Client } from '@aws-sdk/client-s3';
-import { fromNodeProviderChain } from "@aws-sdk/credential-providers";
 
 // Very high level site configuration.
 
@@ -16,8 +16,17 @@ export const siteConfig: metaData = {
 // Default limit to rss feeds and list pages
 export const PAGE_SIZE: number = 10;
 
+// Normally we would use credentials: fromNodeProviderChain().
 // fromNodeProviderChain() attempts to read credentials
 // from a series of standard locations in the runtime environment.
+// But we cannot set the standard environment variables
+// in the AWS Amplify Console because all vars starting
+// with "AWS_" are reserved. Therefore we have to use
+// differently-named environment variables from the standard,
+// so we have to set the credential keys manually like so:
 export const s3client = new S3Client({
-  credentials: fromNodeProviderChain(),
+  credentials: {
+    accessKeyId: safeStringify(process.env.S3_ACCESS_KEY_ID),
+    secretAccessKey: safeStringify(process.env.S3_SECRET_ACCESS_KEY),
+  }
 });
