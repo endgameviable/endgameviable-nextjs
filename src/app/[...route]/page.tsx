a33a58a83acfe6df8151c762017cef43d46f3619
UltrViolet <endgameviable@gmail.com>
2023-11-19 20:21:51 -0500

    fix dynamic route

--- src/app/[...route]/page.tsx
+++ src/app/[...route]/page.tsx
@@ -1,7 +1,8 @@
 import SingleEntryLayout from '@/layouts/entrySingle';
 import EntryListLayout from '@/layouts/entryList';
+import MicroPostEntryLayout from '@/layouts/entryMicropost';
 import Entry from '@/data/interfaces/entry';
-import { fetchJsonFromS3 } from '@/data/s3/fetchFromS3';
+import { JsonDataEndpoint, fetchJsonFromS3 } from '@/data/s3/fetchFromS3';
 import { jsonToEntries, jsonToEntry } from '@/data/s3/jsonToEntry';
 import { getS3Client } from '@config/siteConfig';
 
@@ -50,22 +51,28 @@ function getRoute(pageRoute: string): string {
   return route;
 }
 
-// Renders either a single entry page or a list
-export default async function Page({params}: { params: { route: string[] }}) {
+function getView(jsonData: JsonDataEndpoint): JSX.Element {
   let component: JSX.Element;
-  const startTime = performance.now();
-  const jsonData = await fetchJsonFromS3(getS3Client(), params.route);
-  if (jsonData.metadata.content === 'single') {
+  if (jsonData.metadata.view === 'page') {
     const entry = jsonToEntry(jsonData.pages[0]);
-    component = <SingleEntryLayout entry={entry} />;
+    if (entry.type === 'micropost') {
+      component = <MicroPostEntryLayout entry={entry} />;
+    } else {
+      component = <SingleEntryLayout entry={entry} />;
+    }
   } else {
     const entries = jsonToEntries(jsonData.pages);
     component = <EntryListLayout content="{jsonData.metadata.heading}" list={entries} />;
   }
+  return component;
+}
+
+// Renders either a single entry page or a list
+export default async function Page({params}: { params: { route: string[] }}) {
+  const startTime = performance.now();
+  const jsonData = await fetchJsonFromS3(getS3Client(), params.route);
+  const component = getView(jsonData);
   const elapsed = performance.now() - startTime;
-  // console.log(
-  //   `generated ${params.route.join('/')} page in ${elapsed.toFixed(2)}ms`,
-  // );
   return (
     <main>
       <p>Page was generated in {elapsed.toFixed(2)}ms from json data fetched from S3!</p>
