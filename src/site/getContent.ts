b53bcc2fc4584299a4bde9a9fa43d0254b8a0379
UltrViolet <endgameviable@gmail.com>
2023-11-23 19:26:06 -0500

    prefer s3 over dynamodb

--- src/site/getContent.ts
+++ src/site/getContent.ts
@@ -1,7 +1,7 @@
 import Entry from "@/data/interfaces/entry";
-import { getContentAtRouteDynamo } from "@/data/dynamo/fetchFromDynamo";
 import { getContentAtRouteS3 } from "@/data/s3/fetchFromS3";
+//import { getContentAtRouteDynamo } from "@/data/dynamo/fetchFromDynamo";
 
 export async function getContentAtRoute(route: string[]): Promise<Entry> {
-    return getContentAtRouteDynamo(route);
+    return getContentAtRouteS3(route);
 }
