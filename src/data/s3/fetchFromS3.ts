fe807b98eedd8b4b0361fdfbdb63cd0dc4a3ba29
UltrViolet <endgameviable@gmail.com>
2023-11-23 22:27:08 -0500

    refactor for updated json format

--- src/data/s3/fetchFromS3.ts
+++ src/data/s3/fetchFromS3.ts
@@ -1,24 +1,13 @@
-import { safeStringify } from '@/types/strings';
-import {
-  GetObjectCommand,
-  GetObjectCommandInput,
-  S3Client,
-} from '@aws-sdk/client-s3';
-import { awsAccessKeyId, getS3Client, s3ContentBucketName } from '@config/siteConfig';
 import path from 'path';
 import Entry, { ERROR_ENTRY } from '../interfaces/entry';
+import { safeStringify } from '@/types/strings';
+import { GetObjectCommand, GetObjectCommandInput } from '@aws-sdk/client-s3';
+import { getS3Client, s3ContentBucketName } from '@config/siteConfig';
 import { canonicalizePath } from '@/site/utilities';
-import { jsonToEntries, jsonToEntry } from './jsonToEntry';
 import { safeParseDateMillis } from '@/types/dates';
 import { TextType } from '@/types/contentText';
 
-export interface JsonMetadata {
-  view?: string;
-  section?: string;
-  heading?: string;
-}
-
-export interface JsonDataPage {
+interface HugoJsonPage {
   date?: string; // format: yyyy-mm-ddThh:mm:ss-zzzz
   title?: string;
   summary?: string;
@@ -27,50 +16,7 @@ export interface JsonDataPage {
   type?: string;
   alternates?: string[];
   images?: string[];
-}
-
-export interface JsonDataEndpoint {
-  metadata: JsonMetadata;
-  pages: JsonDataPage[];
-}
-
-// Fetch JSON content data from an S3 bucket source
-export async function fetchJsonFromS3(
-  s3: S3Client,
-  route: string[],
-): Promise<JsonDataEndpoint> {
-  const key = path.join(route.join('/'), 'index.json');
-  const params: GetObjectCommandInput = {
-    Bucket: s3ContentBucketName,
-    Key: key,
-  };
-  try {
-    const response = await s3.send(new GetObjectCommand(params));
-    if (response.Body) {
-      const s = await response.Body?.transformToString();
-      return JSON.parse(s);
-    }
-    return {
-      metadata: { view: 'page', section: 'error' },
-      pages: [
-        {
-          title: 'Nothing Here',
-          content: `<p>There's no content at ${key}.</p>`,
-        },
-      ],
-    };
-  } catch (error) {
-    console.log('error loading file:', error);
-    return {
-      metadata: { view: 'page', section: 'error' },
-      pages: [
-        {
-          title: 'An Error Occurred',
-          content: `<p>There was an error fetching ${key}.</p><p><blockquote>${error}</blockquote></p><p>env: ${safeStringify(process.env.S3_ACCESS_KEY_ID).slice(0,5)}</p><p>window: ${typeof window}</p>`,
-        },
-      ],
-    };
-  }
+  children?: HugoJsonPage[];
 }
 
 export async function getContentAtRouteS3(route: string[]): Promise<Entry> {
@@ -83,21 +29,45 @@ export async function getContentAtRouteS3(route: string[]): Promise<Entry> {
     const response = await getS3Client().send(new GetObjectCommand(params));
     if (response.Body) {
       const s = await response.Body?.transformToString();
-      const data: JsonDataEndpoint = JSON.parse(s);
-      if (data.metadata.view === 'page') {
-        return jsonToEntry(data.pages[0]);
+      const data: HugoJsonPage = JSON.parse(s);
+      if (data.children && data.children.length > 0) {
+        // List page
+        const children = jsonToEntries(data.children);
+        return jsonToEntry(data, children);
       } else {
-        return {
-          timestamp: Date.now(), // TODO: should be section date
-          route: canonicalizePath(route.join('/')),
-          title: data.metadata.section,
-          article: new TextType(data.metadata.heading, 'text/html'),
-          children: jsonToEntries(data.pages),
-        }
+        // Single page
+        return jsonToEntry(data, []);
       }
     }
   } catch (error) {
     console.log(error);
   }
   return ERROR_ENTRY;
+}
+
+// Convert the json returned from S3 endpoints to an Entry
+function jsonToEntry(json: HugoJsonPage, children: Entry[]): Entry {
+  let image: string | undefined = undefined;
+  if (json.images) {
+    image = json.images[0];
+  }
+  return {
+    timestamp: safeParseDateMillis(safeStringify(json.date)),
+    route: canonicalizePath(safeStringify(json.link)),
+    summary: new TextType(safeStringify(json.summary), 'text/plain'),
+    article: new TextType(safeStringify(json.content), 'text/html'),
+    title: json.title,
+    type: json.type,
+    image: image,
+    children: children,
+  };
+}
+
+function jsonToEntries(dataPages: HugoJsonPage[]): Entry[] {
+  const entries: Entry[] = [];
+  for (const jsonPage of dataPages) {
+    const entry = jsonToEntry(jsonPage, []);
+    entries.push(entry);
+  }
+  return entries;
 }
--- src/site/generateLatestEntries.ts
+++ src/site/generateLatestEntries.ts
@@ -1,9 +1,7 @@
 import Entry from '@/data/interfaces/entry';
-import { fetchJsonFromS3 } from '@/data/s3/fetchFromS3';
-import { jsonToEntries } from '@/data/s3/jsonToEntry';
-import { getS3Client } from '@config/siteConfig';
+import { getContentAtRoute } from './getContent';
 
 export async function generateLatestEntries(): Promise<Entry[]> {
-  const jsonData = await fetchJsonFromS3(getS3Client(), []);
-  return jsonToEntries(jsonData.pages);
+  const entry = await getContentAtRoute([]);
+  return entry.children ? entry.children : [];
 }
