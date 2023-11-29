d2cd886a20de667b958e94eaf701ac98fbcc7861
UltrViolet <endgameviable@gmail.com>
2023-11-29 17:35:16 -0500

    remove babel

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
     collectCoverage: true,
     // on node 14.x coverage provider v8 offers good speed and more or less good report
     coverageProvider: 'v8',
@@ -32,15 +34,18 @@ module.exports = {
     },
     // Add more setup options before each test is run
     // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
-    testEnvironment: 'jsdom',
-    transform: {
-      // Use babel-jest to transpile tests with the next/babel preset
-      // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
-      '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-    },
-    transformIgnorePatterns: [
-        '/node_modules/(?!(uuid)/)',
-        '^.+\\.module\\.(css|sass|scss)$',
+    testPathIgnorePatterns: [
+      '<rootDir>/node_modules/',
+      '<rootDir>/.next/'
     ],
+    // testEnvironment: 'jsdom',
+    // transform: {
+    //   // Use babel-jest to transpile tests with the next/babel preset
+    //   // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    //   '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    // },
+    // transformIgnorePatterns: [
+    //     '/node_modules/(?!(uuid)/)',
+    //     '^.+\\.module\\.(css|sass|scss)$',
+    // ],
   }
