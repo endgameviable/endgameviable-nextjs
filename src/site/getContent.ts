5d8f89f670a9965dd88e8b427066fc6d6181f969
UltrViolet <endgameviable@gmail.com>
2023-12-01 08:30:10 -0500

    back to s3 data source

--- src/site/getContent.ts
+++ src/site/getContent.ts
@@ -1,7 +1,7 @@
 import PageContent from '@/data/interfaces/content';
-import { getContentAtRouteLocal } from '@/data/local/fetchFromLocal';
-//import { getContentAtRouteS3 } from '@/data/s3/fetchFromS3';
+//import { getContentAtRouteLocal } from '@/data/local/fetchFromLocal';
+import { getContentAtRouteS3 } from '@/data/s3/fetchFromS3';
 
 export async function getContentAtRoute(route: string[]): Promise<PageContent> {
-    return getContentAtRouteLocal(route);
+    return getContentAtRouteS3(route);
 }
