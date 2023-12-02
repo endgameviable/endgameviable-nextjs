8f1910007823caee6f9483fa2b895b82b1e262ba
UltrViolet <endgameviable@gmail.com>
2023-12-01 23:05:54 -0500

    primitive file concurrency limiting

--- src/data/local/searchLocal.ts
+++ src/data/local/searchLocal.ts
@@ -10,30 +10,37 @@ export async function searchEntriesLocal(
     console.log(`local search starting`);
 
     const files = await walkDirectory(
-        path.join(process.cwd(), 'content'),
-        '',
+        path.join(process.cwd(), 'content'), 
+        '', 
         '.json');
     console.log(`local search found ${files.length} files`);
 
     // There's a limit to the number of files we can open
     // in the Amplify runtime environment.
-    // So I removed the concurrency until or unless
-    // I can find a decent promise concurrency limiter
-    // (pLimit didn't compile).
+    // So we can't run every file promise concurrently.
 
     const matches: PageContent[] = [];
+    const promises: Promise<void>[] = [];
     for (const file of files) {
-        await readLocalJSON(file).then((data) => {
-            const searchable = [
-                data.title?.toLowerCase(),
-                data.plain?.toLowerCase(),
-                data.summary?.toLowerCase(),
-            ].join(' ');
-            if (searchable.includes(params.contains.toLowerCase())) {
-                matches.push(jsonToEntry(data));
-            }
-        });
+        promises.push(
+            readLocalJSON(file).then((data) => {
+                const searchable = [
+                    data.title?.toLowerCase(),
+                    data.plain?.toLowerCase(),
+                    data.summary?.toLowerCase(),
+                ].join(' ');
+                if (searchable.includes(params.contains.toLowerCase())) {
+                    matches.push(jsonToEntry(data));
+                }
+            })
+        );
+        // Primitive concurrency limiting.
+        if (promises.length > 50) {
+            await Promise.all(promises);
+            while (promises.length > 0) promises.pop();
+        }
     }
+    await Promise.all(promises);
 
     if (matches.length > 0) {
         matches.sort((a, b) => b.timestamp - a.timestamp);
@@ -55,26 +62,27 @@ async function walkDirectory(
     dirRoot: string,
     dirPath: string,
     fileExt: string,
-): Promise<string[]> {
+  ): Promise<string[]> {
     const dirs: string[] = [];
     const files: string[] = [];
-
+  
     async function traverse(dirRoot: string, currentDir: string) {
-        dirs.push(currentDir);
-        const items = await fs.readdir(path.join(dirRoot, currentDir));
-
-        for (const item of items) {
-            const itemPath = path.join(currentDir, item);
-            const itemStats = await fs.stat(path.join(dirRoot, itemPath));
-
-            if (itemStats.isDirectory()) {
-                await traverse(dirRoot, itemPath);
-            } else if (itemStats.isFile() && item.endsWith(fileExt)) {
-                files.push(path.join(dirRoot, currentDir, item));
-            }
+      dirs.push(currentDir);
+      const items = await fs.readdir(path.join(dirRoot, currentDir));
+  
+      for (const item of items) {
+        const itemPath = path.join(currentDir, item);
+        const itemStats = await fs.stat(path.join(dirRoot, itemPath));
+  
+        if (itemStats.isDirectory()) {
+          await traverse(dirRoot, itemPath);
+        } else if (itemStats.isFile() && item.endsWith(fileExt)) {
+          files.push(path.join(dirRoot, currentDir, item));
         }
+      }
     }
-
+  
     await traverse(dirRoot, dirPath);
     return files;
-}
+  }
+  
